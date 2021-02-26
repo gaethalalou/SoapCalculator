9310ba4e901922cb4c669c413cedec18145fdea9
Gaeth Alalou <gaethalalou@gmail.com>
2021-02-25 20:27:10 -0700

    Initial commit

--- SoapCalculator/CustomCell.swift
+++ SoapCalculator/CustomCell.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
-
+    
+    @IBOutlet weak var soapView: UIView!
+    @IBOutlet weak var typeImg: UIImageView!
+    @IBOutlet weak var tagLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
